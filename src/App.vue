d30d911bc503cdc8f0b8acac0bfd158127f46fb2
Alfayed Dennita <alfayeddennita@gmail.com>
2023-10-07 17:18:28 +0700

    Create footer

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
 import MainHeader from './components/MainHeader.vue';
 import MainContent from './components/MainContent.vue';
+import MainFooter from './components/MainFooter.vue';
 </script>
 
 <template>
   <MainHeader />
   <MainContent />
+  <MainFooter />
 </template>
 
 <style scoped></style>
---
+++ src/components/MainFooter.vue
@@ -0,0 +1,30 @@
+<template>
+  <footer class="footer">
+    <div class="container footer__container">
+      <p class="footer__text">
+        Built with ❤️ by myself. Copyright &copy; {{ new Date().getFullYear() }}
+      </p>
+    </div>
+  </footer>
+</template>
+
+<style scoped>
+.footer__container {
+  padding-block: 3rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+@media (width >= 768px) {
+  .footer__container {
+    padding-top: 5rem;
+  }
+}
+
+.footer__text {
+  font-size: 0.9rem;
+  text-align: center;
+  color: rgba(var(--text-color-rgb), 0.9);
+}
+</style>
